b611459007bcf6c79763f57e03333e7c8a403552
RyanYuu <taolao12901@gmail.com>
2015-09-23 14:45:02 -0500

    hh

---
+++ join2stringTests.m
@@ -0,0 +1,40 @@
+//
+//  join2stringTests.m
+//  join2stringTests
+//
+//  Created by Huy Vu on 9/16/15.
+//  Copyright (c) 2015 Huy Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface join2stringTests : XCTestCase
+
+@end
+
+@implementation join2stringTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
