f0c7114bde05e7f61350db10e18e47d25d3cf6bf
RyanYuu <taolao12901@gmail.com>
2015-09-23 14:52:47 -0500

    no message

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  join2string
+//
+//  Created by Huy Vu on 9/16/15.
+//  Copyright (c) 2015 Huy Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
